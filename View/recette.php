82f87d9955c14112f1a6206aed89ac5cbcae61ca
cilya <cecile.fischer@coda-student.school>
2025-01-06 15:46:25 +0100

    dgfskf

--- View/recette.php
+++ View/recette.php
@@ -1,68 +1,20 @@
 <?php
-
 ?>
+<h1 class="text-center">Création de recette</h1>
+<form>
+    <div class="mb-3">
+        <label for="recette-name" class="form-label">Nom de la recette</label>
+        <input type="text" class="form-control" id="recette-name" placeholder="Entrez ici le nom de la recette">
+    </div>
+    <select class="form-select" aria-label="Default select example">
+        <option selected>Séléctionnez les </option>
+        <option value="1">One</option>
 
-<form class="row g-3 needs-validation" novalidate>
-    <div class="col-md-4">
-        <label for="validationCustom01" class="form-label">First name</label>
-        <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
-        <div class="valid-feedback">
-            Looks good!
-        </div>
+    </select>
+
+    <div class="mb-3">
+        <label for="recette-description" class="form-label">Préparation</label>
+        <textarea class="form-control" id="recette-description" rows="3"></textarea>
     </div>
-    <div class="col-md-4">
-        <label for="validationCustom02" class="form-label">Last name</label>
-        <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
-        <div class="valid-feedback">
-            Looks good!
-        </div>
-    </div>
-    <div class="col-md-4">
-        <label for="validationCustomUsername" class="form-label">Username</label>
-        <div class="input-group has-validation">
-            <span class="input-group-text" id="inputGroupPrepend">@</span>
-            <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-            <div class="invalid-feedback">
-                Please choose a username.
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <label for="validationCustom03" class="form-label">City</label>
-        <input type="text" class="form-control" id="validationCustom03" required>
-        <div class="invalid-feedback">
-            Please provide a valid city.
-        </div>
-    </div>
-    <div class="col-md-3">
-        <label for="validationCustom04" class="form-label">State</label>
-        <select class="form-select" id="validationCustom04" required>
-            <option selected disabled value="">Choose...</option>
-            <option>...</option>
-        </select>
-        <div class="invalid-feedback">
-            Please select a valid state.
-        </div>
-    </div>
-    <div class="col-md-3">
-        <label for="validationCustom05" class="form-label">Zip</label>
-        <input type="text" class="form-control" id="validationCustom05" required>
-        <div class="invalid-feedback">
-            Please provide a valid zip.
-        </div>
-    </div>
-    <div class="col-12">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-            <label class="form-check-label" for="invalidCheck">
-                Agree to terms and conditions
-            </label>
-            <div class="invalid-feedback">
-                You must agree before submitting.
-            </div>
-        </div>
-    </div>
-    <div class="col-12">
-        <button class="btn btn-primary" type="submit">Submit form</button>
-    </div>
+    <button type="button" class="btn">Créer</button>
 </form>
